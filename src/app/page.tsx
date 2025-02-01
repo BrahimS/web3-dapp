b449f91712f4a880b160628061d4036385526f1b
Sourny <bsourny@gmail.com>
2025-02-01 16:27:59 +0100

    chore:arr hardhat freamworks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <>
-      <h1>Hello Blockchain</h1>
-    </>
-  );
+  return <h1 className="text-3xl font-bold">Hello Blockchain</h1>
+
+
 }
